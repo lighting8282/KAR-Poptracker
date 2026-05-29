5728354e90fb4494f51ee9ef9cfa0251e2f91b53
Lights <96397502+lighting8282@users.noreply.github.com>
2026-05-29 12:34:55 -0400

    Remove dev files and game data from repo, keep only tracker pack files Untrack: all .dat/.h4m game files, .py dev scripts, audio folder, images/Stadiums source images, old (old) icon backups, .vscode, preview screenshots, and other non-pack files. Update .gitignore to prevent re-addition.

--- find_kirby.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-img = Image.open(r"A:\Archipelago\Games\Kirby Air Ride\Pop-Tracker- Kirby Air Ride\GameCube - Kirby Air Ride - Miscellaneous - Kirby.png")
-# Save test crops at different y positions to find the circles
-for y in [230, 260, 290, 310, 330]:
-    crop = img.crop((50, y, 130, y + 70))
-    crop.save(fr"A:\Archipelago\Games\Kirby Air Ride\Pop-Tracker- Kirby Air Ride\images\test_y{y}.png")
-    print(f"Saved test at y={y}")
